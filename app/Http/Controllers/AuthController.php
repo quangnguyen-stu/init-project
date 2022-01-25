df32577f72bfcc98a5418f1cd7282d9576313a10
Toan Nguyen <toan.nguyen@stunited.vn>
2022-01-25 18:36:25 +0700

    cognito demo

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return Inertia::render('Login', [
+            'headerTitle' => 'LOGIN',
+            'title' => 'LOGIN',
+        ]);
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials, $request->remember)) {
+            $request->session()->regenerate();
+
+            return redirect('/home')->with('success', true);
+        }
+        return back()->with('message', 'login fail');
+    }
+
+}
---
+++ resources/js/Home/Login.jsx
@@ -0,0 +1,211 @@
+import { Inertia } from '@inertiajs/inertia'
+import { InertiaLink } from '@inertiajs/inertia-react'
+import {
+  Button,
+  CssBaseline,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Link,
+  Typography,
+  Container,
+  Grid,
+  Snackbar,
+  Card,
+  Box,
+  InputAdornment,
+  IconButton,
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { Visibility, VisibilityOff } from '@material-ui/icons'
+import Alert from '@material-ui/lab/Alert'
+import { useFormik } from 'formik'
+import React, { useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import * as Yup from 'yup'
+
+import { usePageTitle } from '../../libs/usePageTitle'
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(5),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  styledCard: {
+    margin: 'auto',
+    padding: 30,
+    width: 500,
+  },
+  styleContainer: {
+    marginTop: 50,
+  },
+}))
+
+const Copyright = () => (
+  <Typography align="center" color="textSecondary" variant="body2">
+    {'Copyright © '}
+    <Link color="inherit" href="https://stunited.vn/">
+      MSP
+    </Link>{' '}
+    {new Date().getFullYear()}
+    {'.'}
+  </Typography>
+)
+
+export default function Login({ flash }) {
+  const { message: flashMsg } = flash
+  const classes = useStyles()
+  const { title } = usePageTitle()
+  document.title = title
+  const [open, setOpen] = React.useState(false)
+  const [showPassword, setShowPassword] = useState(false)
+  const { t } = useTranslation()
+
+  const LoginForm = Yup.object().shape({
+    email: Yup.string().email(t('VALIDATE.VALID_EMAIL')).required(t('VALIDATE.REQUIRED_EMAIL')),
+    password: Yup.string()
+      .required(t('VALIDATE.REQUIRED_PASSWORD'))
+      .min(6, t('VALIDATE.MIN', { number: '6' })),
+  })
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword)
+  }
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      remember: false,
+    },
+    validationSchema: LoginForm,
+    onSubmit(values, helper) {
+      Inertia.post(route('authenticate'), values, {
+        onError: (errors) => {
+          helper.setFieldValue('password', '')
+          helper.setErrors(errors)
+          setOpen(true)
+        },
+        preserveScroll: (page) => Object.keys(page.props.errors).length,
+      })
+    },
+  })
+
+  const handleClose = (reason) => {
+    if ('clickaway' === reason) {
+      return
+    }
+    setOpen(false)
+  }
+
+  useEffect(() => {
+    if (flashMsg.key) {
+      setOpen(true)
+    }
+  }, [flashMsg])
+
+  return (
+    <Container component="main">
+      <Grid item className={classes.styleContainer} xs={12}>
+        <Card className={classes.styledCard}>
+          <Snackbar
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            autoHideDuration={3000}
+            open={open}
+            onClose={handleClose}
+          >
+            <Alert severity={flashMsg.type} variant="filled">
+              {t(flashMsg.key)}
+            </Alert>
+          </Snackbar>
+
+          <CssBaseline />
+          <div className={classes.paper}>
+            <Typography component="h1" variant="h5">
+              MERCHANT SERVICE PROVIDER
+            </Typography>
+            <form noValidate className={classes.form} onSubmit={formik.handleSubmit}>
+              <TextField
+                autoFocus
+                fullWidth
+                required
+                autoComplete="email"
+                error={formik.touched.email && !!formik.errors.email}
+                helperText={formik.touched.email && formik.errors.email}
+                label="Email"
+                margin="normal"
+                name="email"
+                type="email"
+                value={formik.values.email}
+                variant="outlined"
+                onChange={formik.handleChange}
+              />
+              <TextField
+                fullWidth
+                required
+                autoComplete="current-password"
+                error={formik.touched.password && !!formik.errors.password}
+                helperText={formik.touched.password && formik.errors.password}
+                id="password"
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton aria-label="toggle password visibility" onClick={handleClickShowPassword}>
+                        {showPassword ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+                label="Password"
+                margin="normal"
+                name="password"
+                type={showPassword ? 'text' : 'password'}
+                value={formik.values.password}
+                variant="outlined"
+                onChange={formik.handleChange}
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    color="primary"
+                    id="remember"
+                    value={formik.values.remember}
+                    onChange={formik.handleChange}
+                  />
+                }
+                label="Remember me"
+              />
+              <Button fullWidth className={classes.submit} color="primary" type="submit" variant="contained">
+                Log In
+              </Button>
+              <Grid container>
+                <Link component={InertiaLink} href={route('password.forgot')} variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+            </form>
+          </div>
+        </Card>
+      </Grid>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  )
+}
